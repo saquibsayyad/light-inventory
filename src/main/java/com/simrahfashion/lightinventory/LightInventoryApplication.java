6b693e7804a1873603873758f9a5bf0fb45e5c01
Saquib <saquib777@yahoo.com>
2019-07-27 11:36:57 +0200

    Initialize project

---
+++ src/main/java/com/simrahfashion/lightinventory/LightInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.simrahfashion.lightinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightInventoryApplication.class, args);
+	}
+
+}
